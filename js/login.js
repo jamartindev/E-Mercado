3c88f3104f1ca5ba3c5ebab1c9d0b253dbf2b6c9
Marina <garatmarina@gmail.com>
2023-08-17 00:16:31 -0300

    que redireccione a index.html el login

--- js/login.js
+++ js/login.js
@@ -2,10 +2,20 @@
 
 let datos = localStorage;
 
-document.addEventListener("DOMContentLoaded", function(){
+/*document.addEventListener("DOMContentLoaded", function(){
     document.getElementsByClassName("nombre").addEventListener("focusout", function() {
         datos.setItem("fn", nombre.value);
   })
 })
+*/
        
+
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (document.getElementById("nombre").value && document.getElementById("psw").value) {
+    window.location = "/index.html"
+    localStorage.setItem("usuario", username)
+  }
+})
+
 
