a6542388b568f1b062ef84690702ec7fe33cc772
Toxe98 <111653899+Toxe98@users.noreply.github.com>
2023-08-31 17:09:55 -0300

    filtro y orden

--- js/products.js
+++ js/products.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", async () => {
   // Obtengo la categoria a base del catID de localStorage
   const catID = localStorage.getItem("catID");
-
   // Esto verifica si existe en el localStorage
   if (catID) {
     // Uso el link de la API y cambio el numero del catID por el llamado del catID de localStorage
     const API = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
-
     fetch(API)
       .then((response) => response.json())
       .then((data) => {
@@ -20,35 +18,91 @@ document.addEventListener("DOMContentLoaded", async () => {
     const container = document.getElementById("container");
     let products = json.products;
 
-    for (let i = 0; i < products.length; i++) {
-      console.log(products[i]);
+    // Función para ordenar los productos según el botón seleccionado
+    const sortProducts = (option) => {
+      switch (option) {
+        case "sortAsc":
+          products.sort((a, b) => a.cost - b.cost);
+          break;
+        case "sortDesc":
+          products.sort((a, b) => b.cost - a.cost);
+          break;
+        case "sortByCount":
+          products.sort((a, b) => b.soldCount - a.soldCount);
+          break;
+        default:
+          break;
+      }
+    };
 
-      let name = products[i].name;
-      let description = products[i].description;
-      let cost = products[i].cost;
-      let currency = products[i].currency;
-      let soldCount = products[i].soldCount;
-      let image = products[i].image;
-      const divs = document.createElement("div");
-      divs.innerHTML = `
-      <div class="">
-          <div class="text-bg-dark me-sm-3 pt-5 px-3 pt-md-5 px-md-5">
+    // Función para filtrar los productos según el rango de precios
+    const filterProducts = () => {
+      const minPrice = document.getElementById("rangeFilterCountMin").value;
+      const maxPrice = document.getElementById("rangeFilterCountMax").value;
+      products = products.filter(
+        (product) => product.cost >= minPrice && product.cost <= maxPrice
+      );
+    };
+
+    // Función para mostrar los productos en el contenedor
+    const displayProducts = () => {
+      container.innerHTML = "";
+      for (let i = 0; i < products.length; i++) {
+        let name = products[i].name;
+        let description = products[i].description;
+        let cost = products[i].cost;
+        let currency = products[i].currency;
+        let soldCount = products[i].soldCount;
+        let image = products[i].image;
+        const divs = document.createElement("div");
+        divs.innerHTML = `
+          <div class="">
+            <div class="text-bg-dark me-sm-3 pt-5 px-3 pt-md-5 px-md-5">
               <div class="my-2 py-2">
-                    <div class="d-flex shadow justify-content-between ">
-                      <div class="d-flex">
-                        <img src="${image}" class="p-2" width="250px">
-                        <div class="ms-3">
-                          <p class="h2 fw-normal">${name} - ${currency} ${cost}</p>
-                          <p>${description}</p>
-                        </div>   
-                      </div>   
-                      <small class="me-3 mt-2"> ${soldCount} vendidos</small>
-                    </div>
+                <div class="d-flex shadow justify-content-between ">
+                  <div class="d-flex">
+                    <img src="${image}" class="p-2" width="250px">
+                    <div class="ms-3">
+                      <p class="h2 fw-normal">${name} - ${currency} ${cost}</p>
+                      <p>${description}</p>
+                    </div>   
+                  </div>   
+                  <small class="me-3 mt-2"> ${soldCount} vendidos</small>
+                </div>
               </div>
+            </div>
           </div>
-      </div>
-          `;
-      container.appendChild(divs);
-    }
+        `;
+        container.appendChild(divs);
+      }
+    };
+
+    // Event listener para los botones de ordenar
+    const sortButtons = document.querySelectorAll('[name="options"]');
+    sortButtons.forEach((button) => {
+      button.addEventListener("change", (event) => {
+        sortProducts(event.target.id);
+        displayProducts();
+      });
+    });
+
+    // Event listener para el botón de filtrar
+    const filterButton = document.getElementById("rangeFilterCount");
+    filterButton.addEventListener("click", () => {
+      filterProducts();
+      displayProducts();
+    });
+
+    // Event listener para el botón de limpiar
+    const clearButton = document.getElementById("clearRangeFilter");
+    clearButton.addEventListener("click", () => {
+      document.getElementById("rangeFilterCountMin").value = "";
+      document.getElementById("rangeFilterCountMax").value = "";
+      products = json.products;
+      displayProducts();
+    });
+
+    // Mostrar los productos por defecto
+    displayProducts();
   }
 });
