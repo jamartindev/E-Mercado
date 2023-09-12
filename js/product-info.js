1aab12f72a08df42fe36e3dc81ce3105ba12b4ef
jamartindev <jamarti94@hotmail.com>
2023-09-12 07:20:30 -0300

    DESAFIATE: Comente y reduje un poco el codigo. Agregue funcionalidad para que se guarden los comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -78,13 +78,30 @@ document.addEventListener("DOMContentLoaded", async () => {
     }; getProductDetails();
 
     let responseComments = await getJSONData(PRODUCT_INFO_COMMENTS_URL + productID + EXT_TYPE);
-    let comments = responseComments.data;
-        console.log(comments);
+    let comments;
+
+    /*Función que verifica la existencia en localStorage de los comentarios de un producto según su ID.
+    Si los comentarios no existen en localStorage utiliza lo obtenido de la API directamente.
+
+    En caso de que exista al haberse seteado más abajo, va a utilizar el mismo como valor del array comments.
+    De esta forma se pueden seguir viendo los nuevos comentarios al seguir navegando, sin necesidad de hacer un POST request.*/
+    function getComments() {
+        let verifyComment = localStorage.getItem(`cmnts${productID}`);
+        if (!verifyComment) {
+            comments = responseComments.data;
+            console.log(comments);
+        } else if (verifyComment) {
+            comments = JSON.parse(verifyComment);
+        };
+    }; getComments();
     
 
-    function appendComments() {
-        let commentDivs = document.createElement("div");
+    
+    let commentDivs = document.createElement("div");
         productInfo.appendChild(commentDivs);
+
+    // Función que anexa los comentarios y puntuaciones en pantalla según valores del array comentarios. 
+    function appendComments() {
         
         commentDivs.innerHTML += "<h3 class='mt-5'> Comentarios </h3>"
         for (let i = 0; i < comments.length; i++) {
@@ -124,96 +141,114 @@ document.addEventListener("DOMContentLoaded", async () => {
                 `;
             };
             
-            
+    }; appendComments();
 
-            function commentBox() {
-                let commentsBox = document.createElement("div");
-                productInfo.appendChild(commentsBox);
-                commentsBox.innerHTML = `
-                
-                <div class="tuComentario" style="padding: 30px">
- 	<h3 class='mt-4 mb-4'> Escribe tu comentario </h3>
-        <div class="containerBubble" style="display: inline-flex;
-        	flex-direction: row;
-                align-content: flex-end;
-                align-items: flex-end;">
-                <div class="bubble left">
-                    <div class="comentariosBubble">
-                        <label class='mt-2'> Tu opinión </label>
-                            <input type='text' id="commentUser">
-                            <textarea id="txtComment" name="comentarios" placeholder="Escribe aquí tu comentario"></textarea>
-                            <label> Tu puntuación</label>
-                            <input type="range" id="userScore" min="0" max="5">
-                            <p id="showScore" class="m-0"></p>
-                            <div class="botonComentarios m-0">
-                                <button id="commentBtn" class="btnComentarios">Enviar </button>
-                            </div>
-                    </div>
-                </div>
-          </div>
-</div>
-
-                `
-
-            }; commentBox();
-            
-                function getNewComment() {
-                    let userN = document.getElementById('commentUser');
-                    let rangeScore = document.getElementById('userScore');
-                    let showUserScore = document.getElementById('showScore');
-                    let user = localStorage.getItem("User");
-                    let userCmnt = document.getElementById('txtComment');
-                    let currentDate = new Date().toJSON().slice(0, 10);
-                    let currentHour = new Date().getHours();
-                    let currentMinute = new Date().getMinutes();
-                    let currentSeconds = new Date().getSeconds();
-            
-
-                    
-                    userN.value = user;
-                    userN.disabled = true;
-                    rangeScore.addEventListener('input', () =>{
-                    showUserScore.innerText = rangeScore.value;
-                    });
-            
-                    function pushNewComment() {
-                    
-                        let newComment = {
-                            "product": productID,
-                            "score": rangeScore.value,
-                            "description": userCmnt.value,
-                            "user": userN.value,
-                            "dateTime": `${currentDate}  ${currentHour}:${currentMinute}:${currentSeconds}`
-                        };
-                        
-                        let newCommentDiv = document.createElement("div");
-                        commentDivs.appendChild(newCommentDiv)
-                        newCommentDiv.innerHTML = `
-                        <div class="comentariosGenerales"
-                            <div class="containerBubble" style="display: inline-flex; flex-direction: row; align-content: flex-end; align-items: flex-end;">
-                                <img src="img/img_perfil.png" alt="" class="profile pb-4">
-                                <div class="bubble left">
-                                    <div class='fw-bold fs-5 d-block d-md-flex'>
-                                        ${newComment.user}
-                                        <div class='ps-md-4'> 
-                                            ${newComment.score}
-                                        </div>
+    //Se crea la caja de comentarios y se anexa al div principal.
+    function commentBox() {
+        let commentsBox = document.createElement("div");
+        productInfo.appendChild(commentsBox);
+        commentsBox.innerHTML = `
+        
+        <div class="tuComentario" style="padding: 30px">
+            <h3 class='mt-4 mb-4'> Escribe tu comentario </h3>
+                <div class="containerBubble" style="display: inline-flex;
+                    flex-direction: row;
+                        align-content: flex-end;
+                        align-items: flex-end;">
+                        <div class="bubble left">
+                            <div class="comentariosBubble">
+                                <label class='mt-2'> Tu opinión </label>
+                                    <input type='text' id="commentUser">
+                                    <textarea id="txtComment" name="comentarios" placeholder="Escribe aquí tu comentario"></textarea>
+                                    <label> Tu puntuación</label>
+                                    <input type="range" id="userScore" min="0" max="5">
+                                    <p id="showScore" class="m-0"></p>
+                                    <div class="botonComentarios m-0">
+                                        <button id="commentBtn" class="btnComentarios">Enviar </button>
                                     </div>
-                                        <div class='small'>
-                                            ${newComment.dateTime}
-                                        </div>
-                                        <div class='fst-italic'>
-                                            ${newComment.description}
-                                        </div>
                             </div>
                         </div>
-                    `
+                </div>
+        </div>
 
-                    userCmnt.value = ""
+        `
 
-                    };
-                    document.getElementById('commentBtn').addEventListener('click', pushNewComment);
-                }; getNewComment();
+    }; commentBox();
 
-    }; appendComments();
+    //Funcion que obtiene diversos datos para inicializar la carga del nuevo comentario.
+    function getNewComment() {
+
+        let userN = document.getElementById('commentUser');
+        let rangeScore = document.getElementById('userScore');  // Refs a los inputs del usuario 
+        let showUserScore = document.getElementById('showScore'); 
+        let userCmnt = document.getElementById('txtComment');
+
+        let user = localStorage.getItem("User"); //Tomo el ID del usuario desde el localStorage
+        
+        let currentDate = new Date().toJSON().slice(0, 10); //Func fecha con func de strings slice para quitar las partes que no queremos mostrar.
+        let currentHour = new Date().getHours();    //Funcionalidades de fecha que toman día, hora, min, sec.
+        let currentMinute = new Date().getMinutes();
+        let currentSeconds = new Date().getSeconds();
+
+        userN.value = user;  
+        userN.disabled = true; // En lugar de que usuario ingrese nombre para comentar, se ingresa su id desde el localStorage y se deshabilita el campo.
+        rangeScore.addEventListener('input', () =>{ //Asigna a un range un evento de escucha según input para la calificación del producto. 
+        showUserScore.innerText = rangeScore.value; //Muestra en pantalla el valor mientras se elige
+        });
+
+        //Func que crea nuevo comment. Pushea a un array y permite guardar el comentario de cada producto mientras se navega.
+        function pushNewComment() {
+            
+            //Crea un objeto dinámico de acuerdo a los distintos valores que se obtengan.
+            let newComment = {
+                "product": Number(productID),
+                "score": Number(rangeScore.value),
+                "description": userCmnt.value,
+                "user": userN.value,
+                "dateTime": `${currentDate}  ${currentHour}:${currentMinute}:${currentSeconds}`
+            };
+
+            //Blanqueo div para actualizar comentarios
+            commentDivs.innerHTML = ``;
+
+            //Push al array con los comentarios
+            comments.push(newComment);
+
+            //Seteo en localStorage cada objeto según su ID. Como solo maneja strings, le doy stringify.
+            localStorage.setItem(`cmnts${productID}`, JSON.stringify(comments));
+
+            //LLamo a la función de los comentarios otra vez.
+            appendComments();
+
+        userCmnt.value = ""
+
+        };
+        document.getElementById('commentBtn').addEventListener('click', pushNewComment);
+    }; getNewComment();
 });
+
+
+//Cacho que arranqué para probar lo mismo con menos código
+
+/*let newCommentDiv = document.createElement("div");
+            commentDivs.appendChild(newCommentDiv)
+            newCommentDiv.innerHTML = `
+            <div class="comentariosGenerales"
+                <div class="containerBubble" style="display: inline-flex; flex-direction: row; align-content: flex-end; align-items: flex-end;">
+                    <img src="img/img_perfil.png" alt="" class="profile pb-4">
+                    <div class="bubble left">
+                        <div class='fw-bold fs-5 d-block d-md-flex'>
+                            ${newComment.user}
+                            <div class='ps-md-4'> 
+                                ${newComment.score}
+                            </div>
+                        </div>
+                            <div class='small'>
+                                ${newComment.dateTime}
+                            </div>
+                            <div class='fst-italic'>
+                                ${newComment.description}
+                            </div>
+                </div>
+            </div>
+        `*/
