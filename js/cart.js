a3c681741fa6a9fe914e014d1ed67fa7614e7840
Toxe98 <111653899+Toxe98@users.noreply.github.com>
2023-10-04 14:43:40 -0300

    punto 1

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,24 @@
 
-let carrito = "https://japceibal.github.io/emercado-api/user_cart/25801.json"
-console.log(carrito)
-
-fetch(carrito)
-.then((response) => response.json())
-.then((data) => {
-  console.log(data)
-});
+  let url = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
+  fetch(url)
+  .then(res => res.json())
+  .then(data => mostrarData(data))
+  .catch(error => console.log(error))
+  const mostrarData = (data) =>{
+    console.log(data)
+    let body = '';
+    for (let i = 0; i < data.length; i++){
+      body += `
+        <tr>
+        <td>${data[i].articles.image} ${data[i].articles.name}</td>
+        <td>${data[i].articles.currency} ${data[i].articles.unitCost}</td>
+        <td>${data[i].articles.currency} ${data[i].articles.unitCost}</td>
+        <td>${data[i].articles.count}</td>
+        </tr>
+      `;
+    }
+    document.getElementById('contenidoCarrito').innerHTML = body
+  }
 
 // Esta a medio camino pero solo falta mostrar los datos como cualquier fetch
 // Ya tiene una funcion de onclick que me redirije al carrito
