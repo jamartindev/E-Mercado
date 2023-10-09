8d57312fd27d555c7d8fc2b631cd1d6bafe49256
jamartindev <jamarti94@hotmail.com>
2023-10-09 10:26:35 +0100

    responsive barra nav -products -login y otros cambios peques

--- js/cart.js
+++ js/cart.js
@@ -1,24 +1,29 @@
-
-  let url = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
-  fetch(url)
+let url = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
+// Fetch a la url para obtener los datos
+fetch(url)
   .then(res => res.json())
-  .then(data => mostrarData(data))
+  .then(data => mostrarData(data.articles))
   .catch(error => console.log(error))
-  const mostrarData = (data) =>{
-    console.log(data)
-    let body = '';
-    for (let i = 0; i < data.length; i++){
-      body += `
-        <tr>
-        <td>${data[i].articles.image} ${data[i].articles.name}</td>
-        <td>${data[i].articles.currency} ${data[i].articles.unitCost}</td>
-        <td>${data[i].articles.currency} ${data[i].articles.unitCost}</td>
-        <td>${data[i].articles.count}</td>
-        </tr>
-      `;
-    }
-    document.getElementById('contenidoCarrito').innerHTML = body
-  } 
+// Muestra datos de los articulos que agrego el usuario al carrito
+const mostrarData = (articles) => {
+  console.log(articles);
+  let body = '';
+  for (let i = 0; i < articles.length; i++) {
+    body += `
+      <tr>
+        <td> <img src="${articles[i].image}" width="150vh"> ${articles[i].name}</td>
+        <td>${articles[i].currency} ${articles[i].unitCost}</td>
+        <td><input type="number" min="0" max="100" oninput="Subtotal(${articles[i].unitCost}, this.value, ${i})"></td>
+        <td class="subtotal">${articles[i].currency} <span id="subtotal${i}">${articles[i].unitCost}</span></td>
+        
+      </tr>
+    `;
+  }
+  document.getElementById('contenidoCarrito').innerHTML = body
+};
 
-// Esta a medio camino pero solo falta mostrar los datos como cualquier fetch
-// Ya tiene una funcion de onclick que me redirije al carrito
+// Para multiplicar precio de cantidad de articulos que el usuario vaya a comprar
+let Subtotal = (precioUnit, cantidad, indice) => {
+  const subtotal = precioUnit * cantidad;
+  document.getElementById(`subtotal${indice}`).textContent = subtotal;
+};
